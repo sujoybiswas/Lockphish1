1d45d56e1881bfd04135a04da7d54aa9eb360bdf
Aveng-hacker <61612610+sujoybiswas@users.noreply.github.com>
2020-09-02 14:39:41 +0530

    Update lockphish.sh

--- lockphish.sh
+++ lockphish.sh
@@ -17,7 +17,7 @@ printf "\e[1;33m \____/\___/ \___|_|\_\ \e[0m\e[1;77m.__/|_| |_|_|___/_| |_|\e[0
 printf "\e[1;77m                      |_|                  \e[0m\e[1;33mv1.1\e[0m\n"
 
 printf " \n\e[1;77m coded by: github.com/sujoybiswas/lockphish\e[0m \n"
-printf " \e[1;77mEmail: avengehacker@gmail.com\e[1;77m\e[0m"
+printf " \e[1;77mEmail: avengehacker1@gmail.com\e[1;77m\e[0m"
 printf "\n\n\n\e[1;91m Disclaimer: this tool is designed for security\n"
 printf " testing in an authorized simulated cyberattack\n"
 printf " Attacking targets without prior mutual consent\n"
